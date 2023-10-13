762b925705eb676531fa6c34678c60616b505fa8
HARSHIT RUSIA <79143691+H141019@users.noreply.github.com>
2023-10-14 02:14:04 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,18 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Next Js Project",
   description: "Create My Own Next JS",
+  other: {
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "linkedin:image":
+      "https://png.pngtree.com/element_our/png/20181011/linkedin-social-media-icon-design-template-vector-png_127013.jpg",
+    "linkedin:card": "summary_large_image",
+    "og:url": "project-nxt",
+    "og:image":
+      "https://png.pngtree.com/element_our/png/20181011/linkedin-social-media-icon-design-template-vector-png_127013.jpg",
+
+    "og:type": "website",
+  },
 };
 
 export default function RootLayout({
